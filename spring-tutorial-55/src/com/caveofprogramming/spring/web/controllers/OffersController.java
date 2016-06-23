161e2d7cc2ebbd45376156a21ad327e6542b42fb
Ross Marshall <rossmarshallgithub@gmail.com>
2016-06-23 01:01:43 -0400

    Copped out and used ServletRequestAttributes

--- spring-tutorial-55/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ spring-tutorial-55/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -5,25 +5,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
-
 public class OffersController {
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-@ResponseBody
+	@RequestMapping("/")
 	public String showHome(HttpSession session) {
 	
 		System.out.println( "Debug 1 : session " + session );
 
-		/*if ( session == null ) {
+		if ( session == null ) {
 			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
 			session = attr.getRequest().getSession(true);
-		}*/
+		}
 		
 		session.setAttribute("name", "Boris");
 		
