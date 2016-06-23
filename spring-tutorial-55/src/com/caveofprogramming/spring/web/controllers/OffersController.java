eb185cb4778c6049a7d969edbee22b577f79f54e
Ross Marshall <rossmarshallgithub@gmail.com>
2016-06-22 23:29:46 -0400

    Try more stuff...

--- spring-tutorial-55/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ spring-tutorial-55/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,26 +1,34 @@
 package com.caveofprogramming.spring.web.controllers;
 
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
+
 public class OffersController {
-	
-	@RequestMapping("/")
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+@ResponseBody
 	public String showHome(HttpSession session) {
-		
-		if ( session == null ) {
+	
+		System.out.println( "Debug 1 : session " + session );
+
+		/*if ( session == null ) {
 			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
 			session = attr.getRequest().getSession(true);
-		} 
-			
+		}*/
+		
 		session.setAttribute("name", "Boris");
+		
+		System.out.println( "Debug 1 : session " + session );
 		
 		return "home";
 	}
-
 }
