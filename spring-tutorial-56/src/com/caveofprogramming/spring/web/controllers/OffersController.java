66d3f19ae482035d8f2aeeebc0a775a3f9da1f09
Ross Marshall <rossmarshallgithub@gmail.com>
2016-06-24 21:28:48 -0400

    Spring Data Models and JSTL

--- spring-tutorial-56/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ spring-tutorial-56/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -1,65 +1,56 @@
 package com.caveofprogramming.spring.web.controllers;
 
-import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
+/*
+@Controller
+public class OffersController {
+
+	@RequestMapping("/")
+	public String showHome(HttpSession session) {
+
+		session.setAttribute("name", "Boris");
+
+		return "home";
+	}
+}
+*/
+/*
+@Controller
+public class OffersController {
+
+	@RequestMapping("/")
+	public ModelAndView showHome() {
+
+		ModelAndView mv = new ModelAndView("home");
+		
+		Map<String,Object> model = mv.getModel();
+		
+		model.put("name","<b>River</b>");
+
+		return mv;
+	}
+}
+*/
 
 @Controller
 public class OffersController {
 
-	/*
-	@RequestMapping("/")
-	public String showHome(HttpSession session) {
-		
-		System.out.println( "Debug 1 : session " + session );
-		
-		if ( session == null ) {
-			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-			session = attr.getRequest().getSession(true);
-		}
-		
-		session.setAttribute("name", "Boris");
-		
-		System.out.println( "Debug 1 : session " + session );
-		
-		return "home";
-	}
-	*/
-	/*
-	@RequestMapping("/")
-	public ModelAndView showHome() {
-	
-		ModelAndView mv = new ModelAndView("home");
-		
-		Map<String, Object> model = mv.getModel();
-		
-		model.put("name", "<b>River</b>");
-		
-		return mv;
-	}
-	*/
-	
 	@RequestMapping("/")
 	public String showHome(Model model) {
-		
-	
-		if ( model != null ) {
-			model.addAttribute("name","Tiffany");
-		} else {
-			System.out.println( "showHome : model is null [ " + model +" ]" );
-		}
-		
-		
+
+		model.addAttribute("name", "Tiffany");
+
 		return "home";
 	}
-
 }
