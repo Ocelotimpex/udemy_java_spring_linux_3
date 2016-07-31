464ef8e420b0fbbc1af27d389a6acdbc9ea6cee3
Ross Marshall <rossmarshallgithub@gmail.com>
2016-07-30 22:42:40 -0400

    Outputting Text Based on Authentication Status

---
+++ spring-tutorial-107/src/com/caveofprogramming/spring/web/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showHome() {
+
+		return "home";
+	}
+	
+	@RequestMapping("/admin")
+	public String showAdmin() {
+
+		return "admin";
+	}
+
+}
