cb23eacd8e7b04eef64a7560457e855f4156c6ce
Ross Marshall <rossmarshallgithub@gmail.com>
2016-08-29 22:37:48 -0400

    Checkpoint

---
+++ spring-tutorial-125/test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
@@ -0,0 +1,47 @@
+package com.caveofprogramming.spring.web.test.tests;
+
+
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.caveofprogramming.spring.web.dao.UsersDao;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations = { "classpath:com/caveofprogramming/spring/web/config/dao-context.xml",
+		"classpath:com/caveofprogramming/spring/web/config/security-context.xml",
+		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml"})
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTests {
+	
+	@Autowired
+	private UsersDao usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate( dataSource );
+		
+		
+	}
+	
+	@Test
+	public void testCreateUser() {
+		assertEquals("Dummy test", 1, 1);
+	}
+
+}
