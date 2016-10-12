3b0ca87379f4c81144aef1fd1084a3abff36fce9
Ross Marshall <rossmarshallgithub@gmail.com>
2016-10-11 23:15:25 -0400

    Editing Database Objects with Forms

---
+++ spring-tutorial-133/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,110 @@
+package com.caveofprogramming.spring.web.controllers;
+
+
+import java.security.Principal;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
+
+@Controller
+public class OffersController {
+	
+	private OffersService offersService;	
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+
+	
+	// Call using : http://localhost:8080/offers/test?id=723
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model,@RequestParam("id") String id) {
+
+		System.out.println("ID is: " + id);
+		return "home";
+	}
+	
+	/*
+	@ExceptionHandler(DataAccessException.class)
+	public String handleDatabaseException(DataAccessException ex) {
+		return "error";
+	}
+	*/
+	
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+		
+		//offersService.throwTestException();
+
+		List<Offer> offers = offersService.getCurrent();
+		
+		model.addAttribute("offers", offers);
+
+		return "offers";
+	}
+	/*
+	private Object OffersService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private Object offersService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+*/
+
+	@RequestMapping("/createoffer")
+	public String createOffer(Model model, Principal principal) {
+		
+		Offer offer = null;
+		
+		if ( principal != null ) {
+			String username = principal.getName();
+			
+			System.out.println("DEBUG : OffersConroller : username " + username );
+			
+			offer = offersService.getOffer( username );
+		}
+		
+		if ( offer == null ) {
+			offer = new Offer();
+		}
+		
+		model.addAttribute("offer", offer );
+
+		return "createoffer";
+	}
+	
+	@RequestMapping(value="/docreate", method=RequestMethod.POST)
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result, Principal principal ) { 
+		if (result.hasErrors()) {
+			return "createoffer";			
+		} 
+		
+		String username = principal.getName();
+		System.out.println("DEBUG /docreate username = " + username );
+		
+		offer.getUser().setUsername( username );
+		
+		offersService.saveOrUpdate(offer);
+		
+		return "offercreated";
+	}
+}
