cc5597cfc32f5b96ea288e27a878cbee488ef46e
Ross Marshall <rossmarshallgithub@gmail.com>
2016-10-18 04:28:53 -0400

    testCreateOffer working testUser not working. Getting close.

--- spring-tutorial-137/test/com/caveofprogramming/spring/web/test/tests/OffersDaoTests.java
+++ spring-tutorial-137/test/com/caveofprogramming/spring/web/test/tests/OffersDaoTests.java
@@ -25,7 +25,8 @@ import com.caveofprogramming.spring.web.dao.UsersDao;
 @ContextConfiguration(locations = {
 		"classpath:com/caveofprogramming/spring/web/config/dao-context.xml",
 		"classpath:com/caveofprogramming/spring/web/config/security-context.xml",
-		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
+		"classpath:com/caveofprogramming/spring/web/config/service-context.xml",
+		"classpath:com/caveofprogramming/spring/web/config/datasource.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 
 public class OffersDaoTests {
